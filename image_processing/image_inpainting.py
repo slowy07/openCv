39fe9adc1c8c04ade170584ab97f23611d8d294c
slowy07 <slowy.arfy@gmail.com>
2021-02-16 00:05:37 +0700

    update

--- image_processing/image_inpainting.py
+++ image_processing/image_inpainting.py
@@ -5,12 +5,13 @@ image = cv2.imread('./images/somi.jpg')
 #mask load
 mask_image = cv2.imread('./images/somi.jpg', 0)
 
+#inpaint image function
 inpaint_image = cv2.inpaint(image, mask_image ,2, cv2.INPAINT_NS)
 
 cv2.imshow('result', inpaint_image)
 
 """
-save image
+save images
 cv2.imwrite('result_inpainted', inpaint_image)
 """
 if cv2.waitKey(0) & 0xFF == 27:
