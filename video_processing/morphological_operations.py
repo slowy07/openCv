11b6488744cf93407a7a7053a6dd1f51ea87f554
slowy07 <slowy.arfy@gmail.com>
2021-02-20 10:34:59 +0700

    morphological operations in opencv

---
+++ video_processing/morphological_operations.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+screenRead = cv2.VideoCapture(0)
+
+
+while(1):
+    _, image = screenRead.read()
+    
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    blue1 = np.array([110, 50, 50])
+    blue2 = np.array([130, 255, 255])
+
+
+    mask = cv2.inRange(hsv, blue1, blue2)
+
+    res = cv2.bitwise_and(image, image, mask = mask)
+    kernel = np.ones((5,5), np.uint8)
+
+    opening = cv2.morphologyEx(mas, cv2.MORPH_OPEN, kernel)
+    cv2.imshow('mask', mask)
+    cv2.imshow('opening', opening)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+screenRead.release()
